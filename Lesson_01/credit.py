1d8dfbfb89e9615c3145a9fc320f8378c08e9859
Barsty <barstyle@mail.ru>
2021-07-13 20:44:47 +0300

    Second

--- Lesson_01/credit.py
+++ Lesson_01/credit.py
@@ -3,10 +3,12 @@
 name1 = input('Ввведите имя первого человека: ')
 name2 = input('Ввведите имя второго человека: ')
 name3 = input('Ввведите имя третьего человека: ')
+name4 = input('Ввведите имя четвертого человека: ')
 
 salary1 = input('\nВведите зп первого человека: ')
 salary2 = input('Введите зп второго человека: ')
 salary3 = input('Введите зп третьего человека: ')
+salary4 = input('Введите зп четвертого человека: ')
 
 credit = input('\nВведите сумму кредита: ')
 period = input('Введите срок в месяцах: ')
@@ -18,3 +20,4 @@ print('\nЕжемесячный платеж составит - ', pay_per_month
 print('\n', name1, 'сможет потратить ', int(salary1) - pay_per_month)
 print('\n', name2, 'сможет потратить ', int(salary2) - pay_per_month)
 print('\n', name3, 'сможет потратить ', int(salary3) - pay_per_month)
+print('\n', name4, 'сможет потратить ', int(salary4) - pay_per_month)
