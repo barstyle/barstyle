1552974004396d35626a35f4b2c238e5c945f410
Barsty <barstyle@mail.ru>
2021-07-14 23:43:04 +0300

    home_work

--- Lesson_02/example1.py
+++ Lesson_02/example1.py
@@ -1,1 +1,10 @@
-print('Hello World')
+user_age = input('\nВведите год вашего ррождения :')
+
+while type(user_age) != int:
+    try:
+        user_age = int(user_age)
+    except ValueError:
+        print('Тип вводимых данных должен быть числом')
+        user_age = input('\nПожалуйста, Введите год вашего рождения ЦИФРАМИ :')
+
+print((user_age))
